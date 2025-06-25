ebb6ee696659ee02a2cca5c2b528bc4b59529613
Ajay Pratap Chand <apchand99@gmail.com>
2025-06-25 17:29:30 +0530

    1st responce

--- Lecture 4 Request and Responce/app.js
+++ Lecture 4 Request and Responce/app.js
@@ -3,7 +3,17 @@ console.log("Ajay Pratap Chand");
 const domain = require('http');
 
 const server = domain.createServer((req,res)=>{
+  // res naam ka ek bag aaya esme saaman(responace) bar do mai lete jauga..
   console.log(req.url,req.method,req.headers);
+  res.setHeader('Content-Type','text/html');
+  res.write(<html>
+    <head>
+      <title>Learning Backend</title>
+    </head>
+    <body>
+      <h1>Learing Backend topic Sending Request</h1>
+    </body>
+  </html>)
 });
 
 const PORT = 3001;
